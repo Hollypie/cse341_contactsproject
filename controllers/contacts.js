9d44bc9cb1dcdea38a8983718c20cf154133fba9
Hollypie <hbriggs@byupathway.edu>
2025-09-19 14:15:53 -0600

    trying to get my POST REST client test to work

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -26,7 +26,80 @@ const getSingle = async (req, res) => {
   }
 };
 
+const createContact = async (req, res) => {
+  try {
+    if (
+      !req.body.firstName ||
+      !req.body.lastName ||
+      !req.body.email ||
+      !req.body.favoriteColor ||
+      !req.body.birthday
+    ) {
+      return res.status(400).json('All fields are required');
+    }
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .insertOne(contact);
+
+    if (response.acknowledged) {
+      res.status(201).json({ id: response.insertedId });
+    } else {
+      res.status(500).json('Some error occurred while creating the contact.');
+    }
+  } catch (err) {
+    res.status(500).json(err.message || 'Unexpected error');
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb.getDb().db().collection('contacts').remove({ _id: userId }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+  }
+};
+
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
 };
