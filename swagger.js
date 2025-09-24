d9f13f9e7c08e4ee23729b13ebe1498687af6f26
Hollypie <hbriggs@byupathway.edu>
2025-09-23 19:46:33 -0600

    swagger was only generating api documentation in the local host environment. I attempted to add it to the render site

--- swagger.js
+++ swagger.js
@@ -5,11 +5,12 @@ const doc = {
     title: 'My Contacts API',
     description: 'CSE 341 Project 1 assignment',
   },
-  host: 'localhost:3000',
-  schemes: ['http'],
+  host: process.env.HOST || 'localhost:3000',
+  schemes: process.env.SCHEMES ? process.env.SCHEMES.split(',') : ['http'],
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/contacts.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
+
