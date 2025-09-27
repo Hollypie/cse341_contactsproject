f4f8d1c8c6c2a86c4a0a42b354f6a29e1181ba9e
Hollypie <hbriggs@byupathway.edu>
2025-09-27 10:27:32 -0600

    correcting host to production url

--- swagger.js
+++ swagger.js
@@ -3,14 +3,36 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My Contacts API',
-    description: 'CSE 341 Project 1 assignment',
+    description: 'CSE 341 Project 1 assignment'
   },
-  host: process.env.HOST || 'localhost:3000',
-  schemes: process.env.SCHEMES ? process.env.SCHEMES.split(',') : ['http'],
+  host: 'project1-c5z2.onrender.com',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/contacts.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
+
+
+
+
+
+
+// I needed to hard-code the render host for the host to show my render. 
+// const swaggerAutogen = require('swagger-autogen')();
+
+// const doc = {
+//   info: {
+//     title: 'My Contacts API',
+//     description: 'CSE 341 Project 1 assignment',
+//   },
+//   host: process.env.HOST || 'localhost:3000',
+//   schemes: process.env.SCHEMES ? process.env.SCHEMES.split(',') : ['http'],
+// };
+
+// const outputFile = './swagger.json';
+// const endpointsFiles = ['./routes/contacts.js'];
+
+// swaggerAutogen(outputFile, endpointsFiles, doc);
 
